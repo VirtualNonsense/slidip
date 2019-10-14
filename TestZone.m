6694e016096835d03904b61cf39b9802c07deaad
virtualnonsense <virtualnonsense@mailbox.org>
2019-10-14 16:51:24 +0200

    first working version of houghtransformation

--- TestZone.m
+++ TestZone.m
@@ -1,6 +1,5 @@
-clear all;
-clc;
-close all;
+clear all
+clc
 
 %[file, path, index] = uigetfile({'*.jpg', 'JPEG'; '*.png', 'PNG'}, "please choose a picture");
 filepath = 'C:\Users\andre\OneDrive\Software\source\repos\Matlab\Uni\slidip\testimages\extremtest.jpg'; %strcat(path, file);
@@ -8,7 +7,9 @@ pic = imread(filepath);
 pic = rgb2bw(pic);
 pic = sobeloperator(pic);
 pic = threshold(pic, 0);
-colormap gray;
-image(pic,'CDataMapping', 'scaled');
+pic = houghtransformation(pic, 0.01, 0.1);
+colormap hot;
+imagesc(pic)
+%image(pic,'CDataMapping', 'scaled');
 
 colorbar;
