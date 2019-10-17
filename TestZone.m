8530629a51c6752d3babbc062663a755951f9280
Hedtt <32038803+Hedtt@users.noreply.github.com>
2019-10-17 12:48:15 +0200

    just changed the file path so it works

--- TestZone.m
+++ TestZone.m
@@ -2,13 +2,13 @@ clear all
 clc
 
 %[file, path, index] = uigetfile({'*.jpg', 'JPEG'; '*.png', 'PNG'}, "please choose a picture");
-filepath = 'C:\Users\andre\OneDrive\Software\source\repos\Matlab\Uni\slidip\testimages\extremtest.jpg'; %strcat(path, file);
-pic = imread(filepath);
+%filepath = 'C:\Users\andre\OneDrive\Software\source\repos\Matlab\Uni\slidip\testimages\extremtest.jpg'; %strcat(path, file);
+pic = imread('testimages/lines.jpg');
 pic = rgb2bw(pic);
 pic = sobeloperator(pic);
 pic = threshold(pic, 0);
-pic = houghtransformation(pic, 0.01, 0.1);
-colormap hot;
+pic = houghtransformation(pic, 0.01, 10);
+colormap gray;
 imagesc(pic)
 %image(pic,'CDataMapping', 'scaled');
 
