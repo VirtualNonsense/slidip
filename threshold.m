1ed487b95cbdb00ec3dd959e557d73336f84fb11
Hedtt <32038803+Hedtt@users.noreply.github.com>
2019-10-17 12:46:44 +0200

    changed threshold to be a locigal matrix

--- threshold.m
+++ threshold.m
@@ -1,9 +1,10 @@
 function [outmatrix] = threshold(inmatrix, threshold)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-outmatrix = zeros(size(inmatrix));
-outmatrix(inmatrix >= threshold*max(max(inmatrix(:,:)))) = ...
-    inmatrix(inmatrix >= threshold*max(max(inmatrix(:,:))));
+outmatrix = int32(inmatrix > threshold*max(inmatrix,[],'all'));
+% outmatrix = zeros(size(inmatrix));
+% outmatrix(inmatrix >= threshold*max(max(inmatrix(:,:)))) = ...
+%     inmatrix(inmatrix >= threshold*max(max(inmatrix(:,:))));
 
 
 end
