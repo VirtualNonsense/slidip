303a286c5aab4cff815cad7f34ac4857aa12841b
virtualnonsense <virtualnonsense@mailbox.org>
2019-10-17 15:59:49 +0200

    threshold now returns bool matrix

--- threshold.m
+++ threshold.m
@@ -1,10 +1,10 @@
 function [outmatrix] = threshold(inmatrix, threshold)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-outmatrix = zeros(size(inmatrix));
-outmatrix(inmatrix >= threshold*max(max(inmatrix(:,:)))) = ...
-    inmatrix(inmatrix >= threshold*max(max(inmatrix(:,:))));
-
+%outmatrix = zeros(size(inmatrix));
+% outmatrix(inmatrix >= threshold*max(max(inmatrix(:,:)))) = ...
+%     inmatrix(inmatrix >= threshold*max(max(inmatrix(:,:))));
+outmatrix = inmatrix > threshold * max(inmatrix, [], 'all');
 
 end
 
