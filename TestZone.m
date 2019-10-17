8aee41ba327e9000e8fb44dead48679b07b9776e
virtualnonsense <virtualnonsense@mailbox.org>
2019-10-17 16:01:00 +0200

    using more reasonable pixelsteps to avoid picture artefacts

--- TestZone.m
+++ TestZone.m
@@ -2,13 +2,14 @@ clear all
 clc
 
 %[file, path, index] = uigetfile({'*.jpg', 'JPEG'; '*.png', 'PNG'}, "please choose a picture");
-filepath = 'C:\Users\andre\OneDrive\Software\source\repos\Matlab\Uni\slidip\testimages\extremtest.jpg'; %strcat(path, file);
+filepath = 'C:\Users\andre\OneDrive\Software\source\repos\Matlab\Uni\slidip\testimages\lines.jpg'; %strcat(path, file);
+display("reading image")
 pic = imread(filepath);
 pic = rgb2bw(pic);
 pic = sobeloperator(pic);
 pic = threshold(pic, 0);
-pic = houghtransformation(pic, 0.01, 0.1);
-colormap hot;
+pic = houghtransformation(pic, 0.01, 10);
+colormap hot
 imagesc(pic)
 %image(pic,'CDataMapping', 'scaled');
 
