3259b7ced876eb237521ccadaaa286033f001e09
VirtualNonsense <virtualnonsense@mailbox.org>
2019-12-07 21:46:45 +0100

    added faster but memory expensive houghtransformation

---
+++ alternative_houghtransformation.m
@@ -0,0 +1,33 @@
+function [outmatrix,d,alpha] = alternative_houghtransformation(inmatrix, pistep, dstep)
+%creating empty matrix with dimensions of inmatrix
+    [y,x] = size(inmatrix);
+    
+    %calculating max value for distance from origin (upper left) to lower
+    %right
+    dmax = (x^2 + y^2)^0.5;
+    dmin = -dmax;
+    
+    % defining vectors used with the hough-transformation
+    alpha = 0:pistep:pi;
+    d = dmin:dstep:dmax;
+    outmatrix = zeros(size(alpha,2),size(d,2));
+    % reshaping d in order to exploit FAST MATRIX OPERATIONS \0/
+    d = reshape(d, [1, 1, size(d,2)]);
+    
+    %getting columns and rows where inmatrix is not 0
+    [row,col] = find(inmatrix > 0);
+    pixels = [row,col];
+    
+    % reshaping Pixels in order to exploit FAST MATRIX OPERATIONS \0/
+    pixels = pixels';
+    
+    dist = cos(alpha(:))*pixels(2,:) + sin(alpha(:))*pixels(1,:);
+    
+    diff = abs(dist-d);
+    for index = 1:size(diff,2)
+        slice = reshape(diff(:,index,:), [size(diff, 1), size(diff, 3)]);
+        [~,I] = min(slice,[],2,'linear');
+        outmatrix(I) = outmatrix(I) + 1;
+    end
+end
+
