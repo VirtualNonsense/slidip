c893629b316435261b1b22aee02623c5e19baadd
VirtualNonsense <virtualnonsense@mailbox.org>
2019-12-09 15:17:53 +0100

    reverse houghtransformation is now more efficient by using matlab's matrixoperators

--- hough2lines.m
+++ hough2lines.m
@@ -15,16 +15,14 @@ function [outmatrix] = hough2lines(houghmatrix,picsize,pistep, dstep)
     % initializing empty matrix
     outmatrix = zeros(size(linevector,1),2,2);
 
-    for k = 1:size(linevector,1)
-        %retriving alpha and distance
-        alpha = linevector(k,1)*pistep;
-        d = (linevector(k,2) - d_null)*dstep;
-        % converting d and alpha into point which will be used to draw the line
-        % into the finished picture
-        minpoint = [cos(alpha)*d + tmin*sin(-alpha), sin(alpha)*d+tmin*cos(alpha),];
-        outmatrix(k,:,1) = minpoint;
-        maxpoint = [cos(alpha)*d + tmax*sin(-alpha), sin(alpha)*d+tmax*cos(alpha)];
-        outmatrix(k,:,2) = maxpoint;
-    end
+    %retriving alpha and distance
+    alpha = linevector(:,1)*pistep;
+    d = (linevector(:,2) - d_null)*dstep;
+    % converting d and alpha into point which will be used to draw the line
+    % into the finished picture
+    minpoint = [cos(alpha).*d + tmin*sin(-alpha), sin(alpha).*d+tmin*cos(alpha),];
+    outmatrix(:,:,1) = minpoint;
+    maxpoint = [cos(alpha).*d + tmax*sin(-alpha), sin(alpha).*d+tmax*cos(alpha)];
+    outmatrix(:,:,2) = maxpoint;
 end
 
