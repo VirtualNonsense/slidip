cf175d52dd97bbdb173ea8cffd3ba8dc5fac89f2
VirtualNonsense <virtualnonsense@mailbox.org>
2019-12-10 15:42:08 +0100

    readding getlocalmaxima

---
+++ GetLocalMaxima.m
@@ -0,0 +1,5 @@
+function [maximahough] = GetLocalMaxima(houghmatrix,houghtresholdmatrix)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+    maximahough = double(imregionalmax(houghmatrix.*houghtresholdmatrix));
+end
